8a76ae98abadffdc5ab27c9a867ddfc2e29a0ad1
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-10-13 22:50:01 -0400

    Created hash for names to be converted

---
+++ src/scripts/convert_name.js
@@ -0,0 +1,10 @@
+export const convertName = {
+    "Russian Federation": "Russia",
+    "Bolivia, Plurinational State of": "Bolivia",
+    "Venezuela, Bolivarian Republic of": "Venezuela",
+    "Tanzania, United Republic of": "Tanzania",
+    "Iran, Islamic Republic of": "Iran",
+    "Viet Nam": "Vietnam",
+    "Korea, Republic of": "South Korea",
+    "Lao People's Democratic Republic": "Laos"
+};
