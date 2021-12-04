5075ca24fea26606797291cd41516081b0d63566
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-04 10:33:13 -0500

    Restructure with async function to fetch data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
+import * as d3 from "d3";
 import WorldMap from "./scripts/world_map.js";
-import { modal } from "./scripts/modal";
+import { createModal } from "./scripts/modal";
+import { parseDataByName, parseNameById } from "./scripts/parse_data";
 
-document.addEventListener("DOMContentLoaded", () => {
-    modal();
-    new WorldMap();
+document.addEventListener("DOMContentLoaded", () => {    
+    async function fetchData() {
+        let data;
+        data = await d3.csv("https://raw.githubusercontent.com/rebeccamrfoster/javascript_project_dataset/main/world-happiness-report-2021.csv");
+        const dataByName = parseDataByName(data);
+        
+        data = await d3.tsv("https://raw.githubusercontent.com/KoGor/Map-Icons-Generator/master/data/world-110m-country-names.tsv");
+        const nameById = parseNameById(data);
+
+        createModal();
+        new WorldMap(dataByName, nameById);
+    }
+
+    fetchData();
 });
