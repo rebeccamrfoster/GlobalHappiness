fb60b31a4227c6f42b5e389ba701d100e3082ec4
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-12-09 10:26:34 -0500

    Rearrange imports and replace url with raw data url

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import * as d3 from "d3";
-import WorldMap from "./scripts/world_map.js";
+import { parseDataByName, parseNameById } from "./scripts/parse_data";
 import { createModal } from "./scripts/modal";
 import { createFooter } from "./scripts/footer.js";
-import { parseDataByName, parseNameById } from "./scripts/parse_data";
+import WorldMap from "./scripts/world_map.js";
 
-document.addEventListener("DOMContentLoaded", () => {    
+document.addEventListener("DOMContentLoaded", () => {
     async function fetchData() {
         let data;
-        data = await d3.csv("https://github.com/rebeccamrfoster/GlobalHappinessDataset/blob/main/world-happiness-report-2021.csv");
+        data = await d3.csv("https://raw.githubusercontent.com/rebeccamrfoster/GlobalHappinessDataset/main/world-happiness-report-2021.csv");
         const dataByName = parseDataByName(data);
         
         data = await d3.tsv("https://raw.githubusercontent.com/KoGor/Map-Icons-Generator/master/data/world-110m-country-names.tsv");
         const nameById = parseNameById(data);
-
+        
         createModal();
         createFooter();
         new WorldMap(dataByName, nameById);
